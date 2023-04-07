85d0c6466aabb98511898ba1cde6f8e2129cd9fa
leng hongmeng <lenghongmeng98@gmail.com>
2023-04-07 14:31:24 +0700

    imp

--- src/main/java/com/example/todo_list_management/service/AuthService.java
+++ src/main/java/com/example/todo_list_management/service/AuthService.java
@@ -2,8 +2,7 @@ package com.example.todo_list_management.service;
 
 import com.example.todo_list_management.config.JwtAuthFilter;
 import com.example.todo_list_management.config.JwtUtil;
-import com.example.todo_list_management.exception.InvalidPasswordExceptionHandler;
-import com.example.todo_list_management.exception.NotFoundExceptionHandler;
+import com.example.todo_list_management.exception.*;
 import com.example.todo_list_management.model.entity.UserApp;
 import com.example.todo_list_management.model.request.AuthRequest;
 import com.example.todo_list_management.model.response.AuthResponse;
@@ -11,8 +10,9 @@ import com.example.todo_list_management.repository.UserAppRepository;
 import com.example.todo_list_management.service.serviceImp.UserAppServiceImp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,32 +21,44 @@ public class AuthService {
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
     private final UserAppRepository userAppRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
 
     public AuthResponse authenticate(AuthRequest authRequest) {
 
-        String password = passwordEncoder.encode(authRequest.getPassword());
-        UserApp getUser = userAppRepository.getUserByEmail(authRequest.getEmail());
+        UserApp userApp = userAppRepository.getUserByEmail(authRequest.getEmail());
 
-        if(!password.equals(getUser.getPassword())) {
-            throw new InvalidPasswordExceptionHandler("Invalid password");
+        if(userApp == null) {
+            throw new NotFoundExceptionHandler("User not found");
+        }
+        else if(authRequest.getEmail().isEmpty()) {
+            throw new FieldEmptyExceptionHandler("Email field is empty");
+        }
+        else if(authRequest.getEmail().isBlank()) {
+            throw new FieldBlankExceptionHandler("Email field is blank");
+        }
+        else if(!authRequest.getEmail().matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$")) {
+            throw new NotValidValueExceptionHandler("Email not valid");
         }
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        authRequest.getEmail(),
-                        authRequest.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authRequest.getEmail(),
+                            authRequest.getPassword()
+                    )
+            );
 
-        var user = userAppRepository.getUserByEmail(authRequest.getEmail());
+            var user = userAppRepository.getUserByEmail(authRequest.getEmail());
 
-        var jwtToken = jwtUtil.generateToken(user);
-        return AuthResponse.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .token(jwtToken)
-                .build();
+            var jwtToken = jwtUtil.generateToken(user);
+            return AuthResponse.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .token(jwtToken)
+                    .build();
+
+        } catch (Exception e) {
+            throw new InvalidPasswordExceptionHandler("Invalid password");
+        }
     }
 
 }
